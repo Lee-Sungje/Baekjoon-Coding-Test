a7550c662f1093eff0386d409dc08466e7bff1c8
Lee-Sungje <dtw2813@gmail.com>
2020-09-01 23:49:01 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·Stack(10828) - 10828 스택
    ·WordReversing(9093) - 9093 단어 뒤집기

---
+++ 문자열/WordReversing(9093).cpp
@@ -0,0 +1,34 @@
+﻿/* 백준알고리즘 - 9093 단어 뒤집기 */
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main (void) {
+	int T;
+
+	cin >> T;
+	cin.ignore();
+	while (T--) {
+		string sentence;
+		getline(cin, sentence);
+		sentence += '\n';
+		
+		stack<char> word;
+		for (char ch : sentence) {
+			if (ch == ' ' || ch == '\n') {
+				while (!word.empty()) {
+					cout << word.top();
+					word.pop();
+				}
+				cout << ch;
+			} else {
+				word.push(ch);
+			}
+		}
+	}
+
+	return 0;
+}
