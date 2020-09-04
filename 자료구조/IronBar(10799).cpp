1bba784119142ce240629e8d114f3b88f5c27c50
Lee-Sungje <dtw2813@gmail.com>
2020-09-04 21:54:46 +0900

    백준알고리즘 코딩테스트 연습 문제, 파일 구조 변경
    ·IronBar(10799) - 10799 쇠막대기
    ·Okeunsu(17298) - 17298 오큰수
    
    문자열→자료구조
    ·Editor_Stack(1406) - 1406 에디터(스택으로 구현)
    ·JosephsProblem(1158) - 1158 요세푸스 문제
    ·Parenthesis(9012) - 9012 괄호
    ·Stack(10828) - 10828 스택
    ·StackProgression(1874) - 1874 스택 수열

---
+++ 자료구조/IronBar(10799).cpp
@@ -0,0 +1,33 @@
+﻿/* 백준알고리즘 - 10799 쇠막대기 */
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+	int count = 0;
+	bool isLaser = false;
+	string str;
+	stack<char> stack;
+
+	getline(cin, str);
+	for (char ch : str) {
+		if (ch == '(') {
+			stack.push(ch);
+			isLaser = true;
+		} else if (ch == ')') {
+			stack.pop();
+			if (isLaser) {
+				count += stack.size();
+				isLaser = false;
+			} else {
+				count++;
+			}
+		}
+	}
+	cout << count;
+
+	return 0;
+}
---
+++ 자료구조/Okeunsu(17298).cpp
@@ -0,0 +1,39 @@
+﻿/* 백준알고리즘 - 17298 오큰수 */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+	int n;
+
+	cin >> n;
+
+	vector<int> a(n);
+	vector<int> nge(n, -1);
+	for (int idx = 0; idx < n; idx++)
+		cin >> a[idx];
+
+	stack<int> stack;
+	for (int idx = 0; idx < n - 1; idx++) {
+		while (!stack.empty()) {
+			if (a[stack.top()] < a[idx + 1]) {
+				nge[stack.top()] = a[idx + 1];
+				stack.pop();
+			} else
+				break;
+		}
+
+		if (a[idx] < a[idx + 1])
+			nge[idx] = a[idx + 1];
+		else if (a[idx] >= a[idx + 1])
+			stack.push(idx);
+	}
+
+	for_each(nge.begin(), nge.end(), [](int e) { cout << e << ' '; });
+
+	return 0;
+}
