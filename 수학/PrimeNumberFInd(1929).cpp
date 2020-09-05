90707fdca58fc6de87c7936b3e6bad03986075e4
Lee-Sungje <dtw2813@gmail.com>
2020-09-05 23:06:21 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·Factorial(10872) - 10872 팩토리얼
    ·PrimeNumberFind(1929) - 1929 소수 구하기

---
+++ 수학/PrimeNumberFInd(1929).cpp
@@ -0,0 +1,30 @@
+﻿/* 백준알고리즘 - 1929 소수 구하기 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> SieveOfEratosthenes(int n) {
+	vector<bool> prime(n + 1, true);
+
+	prime[0] = prime[1] = false;
+	for (int i = 2; i * i <= n; i++)
+		for (int j = i * i; j <= n; j += i)
+			prime[j] = false;
+
+	return prime;
+}
+
+int main(void) {
+	int m, n;
+
+	cin >> m >> n;
+	vector<bool> prime = SieveOfEratosthenes(n);
+
+	for (int idx = m; idx <= n; idx++)
+		if (prime[idx])
+			cout << idx << '\n';
+
+	return 0;
+}
