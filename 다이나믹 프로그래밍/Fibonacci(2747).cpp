3ac8ed5552c961bea8ee843af9f4a5943cfb4a61
Lee-Sungje <dtw2813@gmail.com>
2020-09-06 23:16:50 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·Fibonacci(2747) - 2747 피보나치 수
    ·MakeAsOne(1463) - 1463 1로 만들기

---
+++ 다이나믹 프로그래밍/Fibonacci(2747).cpp
@@ -0,0 +1,27 @@
+﻿/* 백준알고리즘 - 2747 피보나치 수 */
+
+#include <iostream>
+
+using namespace std;
+
+const int memory_size = 45;
+int memo[memory_size] = { 0, };
+
+int fibonacci(int n) {
+	if (n <= 1) {
+		return n;
+	} else {
+		if(!memo[n])
+			memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+		return memo[n];
+	}
+}
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	cout << fibonacci(n) << '\n';
+
+	return 0;
+}
