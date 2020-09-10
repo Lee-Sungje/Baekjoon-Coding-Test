cdc0d23f18a2bfbcdf497ff3013ecd876a640712
Lee-Sungje <dtw2813@gmail.com>
2020-09-10 19:43:48 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·CardBuying(11052) - 11052 카드 구매하기
    ·LIS(11053) - 가장 긴 증가하는 부분 수열

---
+++ 다이나믹 프로그래밍/LIS(11053).cpp
@@ -0,0 +1,28 @@
+﻿/* 백준알고리즘 - 11053 가장 긴 증가하는 부분 수열 */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int arr_size = 1001;
+
+int main(void) {
+	int n, ans = 0;
+	int a[arr_size] = { 0, };
+	int d[arr_size] = { 0, };
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		d[i] = 1;
+		for (int j = i - 1; j >= 1; j--) {
+			if (a[i] > a[j])
+				d[i] = max(d[i], d[j] + 1);
+		}
+		ans = ans < d[i] ? d[i] : ans;
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
