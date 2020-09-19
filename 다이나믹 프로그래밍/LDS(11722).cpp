6aabd6edf4239d013080cbdae808f537e5a7bf32
Lee-Sungje <dtw2813@gmail.com>
2020-09-19 23:25:14 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·LDS(11722) - 11622 가장 긴 감소하는 부분 수열
    ·LVS(11054) - 11054 가장 긴 바이토닉 부분 수열

---
+++ 다이나믹 프로그래밍/LDS(11722).cpp
@@ -0,0 +1,26 @@
+﻿/* 백준알고리즘 - 11722 가장 긴 감소하는 부분 수열 */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[1000];
+int d[1000];
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		d[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (a[i] < a[j] && d[i] < d[j] + 1)
+				d[i] = d[j] + 1;
+		}
+	}
+	cout << *max_element(d, d + n) << '\n';
+
+	return 0;
+}
