162c54c9c5ef2e93966f39f15195384e3ae11106
Lee-Sungje <dtw2813@gmail.com>
2020-09-20 17:24:18 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·13398_연속합 2 - https://www.acmicpc.net/problem/13398
    ·2133_타일 채우기 - https://www.acmicpc.net/problem/2133

---
+++ 다이나믹 프로그래밍/2133_타일 채우기.cpp
@@ -0,0 +1,25 @@
+﻿/* BOJ - 2133 타일 채우기 */
+
+#include <iostream>
+
+using namespace std;
+
+int d[31] = { 1 };
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	if (n % 2)
+		d[n] = 0;
+	else {
+		for (int i = 2; i <= n; i += 2) {
+			d[i] = d[i - 2] * 3;
+			for (int j = i - 2; j > 0; j -= 2)
+				d[i] += d[j - 2] * 2;
+		}
+	}
+	cout << d[n] << '\n';
+
+	return 0;
+}
