1fc0fe64b34a9801baab651a0d86e79943c75ce3
Lee-Sungje <dtw2813@gmail.com>
2020-09-21 16:49:35 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1309_동물원(Refactoring) - https://www.acmicpc.net/problem/1309
    ·17404_RGB거리 2 - https://www.acmicpc.net/problem/17404

---
+++ 다이나믹 프로그래밍/1309_동물원(Refactoring).cpp
@@ -0,0 +1,22 @@
+﻿/* BOJ - 1309 동물원(Refactoring) */
+
+#include <iostream>
+
+using namespace std;
+
+const int mod = 9901;
+int d[100'001] = { 1, 2 };
+int s[100'001] = { 1, 3 };
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		d[i] = (d[i - 1] + 2 * s[i - 2]) % mod;
+		s[i] = (d[i] + s[i - 1]) % mod;
+	}
+	cout << s[n] << '\n';
+
+	return 0;
+}
---
+++ 다이나믹 프로그래밍/17404_RGB거리 2.cpp
@@ -0,0 +1,47 @@
+﻿/* BOJ - 17404 RGB거리 2 */
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+const int r = 0;
+const int g = 1;
+const int b = 2;
+const int maxVal = 1000 * 1000 + 1;
+
+int a[3][1001];
+int d[3][1001];
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		scanf_s("%d %d %d", &a[r][i], &a[g][i], &a[b][i]);
+
+	int ans = maxVal;
+	for (int k = 0; k < 3; k++) {
+		for (int i = 0; i < 3; i++) {
+			if (i == k)
+				d[i][1] = a[i][1];
+			else
+				d[i][1] = maxVal;
+		}
+
+		for (int i = 2; i <= n; i++) {
+			d[r][i] = min(d[g][i - 1], d[b][i - 1]) + a[r][i];
+			d[g][i] = min(d[r][i - 1], d[b][i - 1]) + a[g][i];
+			d[b][i] = min(d[r][i - 1], d[g][i - 1]) + a[b][i];
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if (i == k)		continue;
+			ans = min(ans, d[i][n]);
+		}
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
