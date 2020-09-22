fa9e022fee7b40bd677d6e94847a1e00fc244aa4
Lee-Sungje <dtw2813@gmail.com>
2020-09-22 23:47:34 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·2225_합분해(Refactoring) - https://www.acmicpc.net/problem/2225
    ·2309_일곱 난쟁이 - https://www.acmicpc.net/problem/2309

---
+++ 다이나믹 프로그래밍/2225_합분해(Refactoring).cpp
@@ -0,0 +1,22 @@
+﻿/* BOJ - 2225 합분해(Refactoring) */
+
+#include <iostream>
+
+using namespace std;
+
+const int mod = 1'000'000'000;
+int d[201][201];
+
+int main(void) {
+	int n, k;
+
+	cin >> n >> k;
+	for (int i = 1; i <= k; i++) {
+		d[i][1] = i;
+		for (int j = 2; j <= n; j++)
+			d[i][j] = (d[i][j - 1] + d[i - 1][j]) % mod;
+	}
+	cout << d[k][n] << '\n';
+
+	return 0;
+}
---
+++ 브루트 포스/2309_일곱 난쟁이.cpp
@@ -0,0 +1,30 @@
+﻿/* BOJ - 2309 일곱 난쟁이 */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int n = 9;
+int a[n];
+
+int main(void) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		sum += a[i];
+	}
+
+	sort(a, a + n);
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (sum - (a[i] + a[j]) == 100) {
+				for (int k = 0; k < n; k++) {
+					if (k == i || k == j)	continue;
+					cout << a[k] << '\n';
+				}
+				return 0;
+			}
+		}
+	}
+}
