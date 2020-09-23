6870baabd1be7c06b7b5b8d3410a98a0c839f615
Lee-Sungje <dtw2813@gmail.com>
2020-09-23 22:48:02 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1476_날짜 계산 - https://www.acmicpc.net/problem/1476
    ·3085_사탕 게임 - https://www.acmicpc.net/problem/3085

---
+++ 브루트 포스/1476_날짜 계산.cpp
@@ -0,0 +1,26 @@
+﻿/* BOJ - 1476 날짜 계산 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int e, s, m;	
+	cin >> e >> s >> m;
+
+	int year = 1;
+	bool isBreak = false;
+	for (int i = 1, j = 1, k = 1; !isBreak; i++, j++, k++) {
+		if (i > 15) i = 1;
+		if (j > 28) j = 1;
+		if (k > 19) k = 1;
+
+		if (i == e && j == s && k == m)
+			isBreak = true;
+		else
+			year++;
+	}
+	cout << year << '\n';
+
+	return 0;
+}
---
+++ 브루트 포스/3085_사탕 게임.cpp
@@ -0,0 +1,68 @@
+﻿/* BOJ - 3085 사탕 게임 */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int check(vector<string> &a) {
+	int ans = 0;
+
+	int n = a.size();
+	for (int i = 0; i < n; i++) {
+		int count = 1;
+		for (int j = 1; j < n; j++) {
+			if (a[i][j] == a[i][j - 1])
+				count++;
+			else
+				count = 1;
+
+			if (ans < count) ans = count;
+		}
+
+		count = 1;
+		for (int j = 1; j < n; j++) {
+			if (a[j][i] == a[j - 1][i])
+				count++;
+			else
+				count = 1;
+
+			if (ans < count) ans = count;
+		}
+	}
+
+	return ans;
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+	vector<string> a(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int temp;
+			if (j + 1 < n) {
+				swap(a[i][j], a[i][j + 1]);
+				temp = check(a);
+				ans = ans < temp ? temp : ans;
+				swap(a[i][j], a[i][j + 1]);
+			}
+
+			if (i + 1 < n) {
+				swap(a[i][j], a[i + 1][j]);
+				temp = check(a);
+				ans = ans < temp ? temp : ans;
+				swap(a[i][j], a[i + 1][j]);
+			}
+		}
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
