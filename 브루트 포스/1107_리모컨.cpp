0472a9af2038f61f86a309ab2099507a7d1e95ee
Lee-Sungje <dtw2813@gmail.com>
2020-09-24 20:54:31 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1107_리모컨 - https://www.acmicpc.net/problem/1107
    ·14500_테트로미노 - https://www.acmicpc.net/problem/14500

---
+++ 브루트 포스/1107_리모컨.cpp
@@ -0,0 +1,58 @@
+﻿/* BOJ - 1107 리모컨 */
+
+#include <iostream>
+
+using namespace std;
+
+bool broken[10];
+
+int possible(int c) {
+	int count = 0;
+
+	if (c == 0) {
+		if (!broken[c])
+			return 1;
+		else
+			return -1;
+	} else if (c == 100) {
+		return 0;
+	} else {
+		while (c) {
+			if (!broken[c % 10])
+				count++;
+			else
+				return -1;
+			c /= 10;
+		}
+		return count;
+	}
+}
+
+int main(void) {
+	int n, m;
+
+	cin >> n;
+	cin >> m;	
+	while (m--) {
+		int i;
+		cin >> i;
+		broken[i] = true;
+	}
+
+	int ans = 500'000;
+	for (int i = 0; i < 1'000'000; i++) {
+		int c = i;
+		int temp = possible(c);
+
+		if (temp != -1)
+			temp += abs(c - n);
+		else 
+			continue;
+		
+		if (ans > temp)
+			ans = temp;
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
---
+++ 브루트 포스/14500_테트로미노.cpp
@@ -0,0 +1,71 @@
+﻿/* BOJ - 14500 테트로미노 */
+
+#include <iostream>
+
+using namespace std;
+
+const int block[19][4][2] = {
+	{{0, 0}, {0, 1}, {1, 0}, {1, 1}}, // O
+	{{0, 0}, {0, 1}, {0, 2}, {0, 3}}, // I
+	{{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+	{{0, 0}, {0, 1}, {0, 2}, {1, 1}}, // T
+	{{0, 1}, {1, 0}, {1, 1}, {1, 2}},
+	{{0, 0}, {1, 0}, {1, 1}, {2, 0}},
+	{{0, 1}, {1, 0}, {1, 1}, {2, 1}},
+	{{0, 0}, {1, 0}, {1, 1}, {2, 1}}, // Z
+	{{0, 1}, {1, 0}, {1, 1}, {2, 0}}, 
+	{{0, 1}, {0, 2}, {1, 0}, {1, 1}}, 
+	{{0, 0}, {0, 1}, {1, 1}, {1, 2}}, 
+	{{0, 0}, {1, 0}, {2, 0}, {2, 1}}, // L
+	{{0, 1}, {1, 1}, {2, 0}, {2, 1}},
+	{{0, 0}, {0, 1}, {0, 2}, {1, 0}},
+	{{0, 0}, {0, 1}, {0, 2}, {1, 2}},
+	{{0, 0}, {0, 1}, {1, 1}, {2, 1}},
+	{{0, 0}, {0, 1}, {1, 0}, {2, 0}},
+	{{0, 0}, {1, 0}, {1, 1}, {1, 2}},
+	{{0, 2}, {1, 0}, {1, 1}, {1, 2}}
+};
+
+int a[500][500];
+int n, m;
+
+int tetromino(int x, int y) {
+	int ans = 0;
+
+	for (int i = 0; i < 19; i++) {
+		int weight = 0;
+		for (int j = 0; j < 4; j++) {
+			int current_x = x + block[i][j][0];
+			int current_y = y + block[i][j][1];
+
+			if (current_x < n && current_y < m)
+				weight += a[current_x][current_y];
+			else
+				break;
+		}
+		if (ans < weight)
+			ans = weight;
+	}
+
+	return ans;
+}
+
+int main(void) {	
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			scanf_s("%d", &a[i][j]);
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		int temp = 0;
+		for (int j = 0; j < m; j++) {
+			temp = tetromino(i, j);
+			if (ans < temp)
+				ans = temp;
+		}
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
