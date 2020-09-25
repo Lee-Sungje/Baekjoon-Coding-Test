2c50ea6c0537f561a2acf71749d030e510b131f5
Lee-Sungje <dtw2813@gmail.com>
2020-09-25 19:25:29 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1748_수 이어 쓰기 1 - https://www.acmicpc.net/problem/1748
    ·6064_카잉 달력 - https://www.acmicpc.net/problem/6064

---
+++ 브루트 포스/1748_수 이어 쓰기 1.cpp
@@ -0,0 +1,23 @@
+﻿/* BOJ - 1748 수 이어 쓰기 1 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	int ans = 0, k = 1;
+	for (int i = 1; k <= n; i++) {
+		if (k * 10 <= n)
+			ans += i * (k * 10 - k);
+		else
+			ans += i * (n - k + 1);	
+		
+		k *= 10;
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
---
+++ 브루트 포스/6064_카잉 달력.cpp
@@ -0,0 +1,30 @@
+﻿/* BOJ - 6064 카잉 달력 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int t;
+	int m, n, x, y;
+
+	cin >> t;
+	while (t--) {
+		cin >> m >> n >> x >> y;
+
+		bool is_valid = false;
+		x -= 1;
+		y -= 1;
+		for (int k = x; k < (m*n); k += m) {
+			if (k % n == y) {
+				cout << k + 1 << '\n';
+				is_valid = true;
+				break;
+			}
+		}
+		if (!is_valid)
+			cout << -1 << '\n';
+	}
+
+	return 0;
+}
