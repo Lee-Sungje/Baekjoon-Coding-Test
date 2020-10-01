7225ed60512bc5b9c964d1a6a0572875a422162c
Lee-Sungje <dtw2813@gmail.com>
2020-10-01 23:01:17 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·9095_1, 2, 3 더하기(Recursive) - https://www.acmicpc.net/problem/9095

---
+++ 브루트 포스/9095_1, 2, 3 더하기(Recursive).cpp
@@ -0,0 +1,32 @@
+﻿/* BOJ - 9095 1, 2, 3 더하기(Recursive) */
+
+#include <iostream>
+
+using namespace std;
+
+int go(int sum, int goal) {
+	if (sum > goal)	return 0;
+	if (sum == goal) return 1;
+	int now = 0;
+	for (int i = 1; i <= 3; i++)
+		now += go(sum + i, goal);
+
+	return now;
+}
+
+void solve() {
+	int n;
+	cin >> n;
+
+	cout << go(0, n) << '\n';
+}
+
+int main(void) {
+	int t;
+	cin >> t;
+
+	while (t--)
+		solve();
+
+	return 0;
+}
