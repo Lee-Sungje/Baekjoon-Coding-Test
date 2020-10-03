cdbe8d2af17d342884397953c7734af5a81e5513
Lee-Sungje <dtw2813@gmail.com>
2020-10-04 00:18:08 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·14501_퇴사 - https://www.acmicpc.net/problem/14501

---
+++ 브루트 포스/14501_퇴사.cpp
@@ -0,0 +1,38 @@
+﻿/* BOJ - 14501 퇴사 */
+
+#include <iostream>
+
+using namespace std;
+
+int t[16];
+int p[16];
+int n;
+int answer = 0;
+
+void go(int day = 1, int sum = 0) {
+	if (day == n + 1) {
+		if (answer < sum) answer = sum;
+		return;
+	}
+	if (day > n + 1) {
+		return;
+	}
+	go(day + 1, sum);
+	go(day + t[day], sum + p[day]);
+}
+
+void solve() {
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+		cin >> t[i] >> p[i];
+	
+	go();
+	cout << answer << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
