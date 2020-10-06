10b7c2decbdb64f305727a88e34fbd752552fdb7
Lee-Sungje <dtw2813@gmail.com>
2020-10-06 22:12:23 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1248_맞춰봐 - https://www.acmicpc.net/problem/1248
    ·2529_부등호(Refactoring) - https://www.acmicpc.net/problem/2529

---
+++ 브루트 포스/1248_맞춰봐.cpp
@@ -0,0 +1,61 @@
+﻿/* BOJ - 1248 맞춰봐 */
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+char s[10][10];
+int answer[10];
+
+bool check(int index) {
+	int sum = 0;
+	for (int i = index; i >= 0; i--) {
+		sum += answer[i];
+		if (s[i][index] == '0') {
+			if (sum != 0)	return false;
+		} else if (s[i][index] == '-') {
+			if (sum >= 0)	return false;
+		} else if (s[i][index] == '+') {
+			if (sum <= 0)	return false;
+		}
+	}
+	return true;
+}
+
+bool go(int index = 0) {
+	if (index == n) return true;
+	if (s[index][index] == '0') {
+		answer[index] = 0;
+		return check(index) && go(index + 1);
+	}
+
+	for (int i = 1; i <= 10; i++) {
+		if (s[index][index] == '-')
+			answer[index] = i * (-1);
+		else if (s[index][index] == '+')
+			answer[index] = i;
+
+		if (check(index) && go(index + 1)) return true;
+	}
+	return false;
+}
+
+void solve() {
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		for (int j = i; j < n; j++)
+			cin >> s[i][j];
+
+	go();
+
+	for (int i = 0; i < n; i++)
+		cout << answer[i] << ' ';
+	cout << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 브루트 포스/2529_부등호(Refactoring).cpp
@@ -0,0 +1,56 @@
+﻿/* BOJ - 2529 부등호(Refactoring) */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+char a[10];
+bool check[10];
+vector<string> answer;
+
+bool valid_check(char x, char y, char op) {
+	if (op == '>') {
+		if (x < y)	return false;
+	} else if (op == '<') {
+		if (x > y)	return false;
+	}
+
+	return true;
+}
+
+void go(int index = 0, string number = "") {
+	if (index == n + 1) {
+		answer.push_back(number);
+		return;
+	}
+
+	for (int i = 0; i <= 9; i++) {
+		if (check[i])	continue;
+		if (index == 0 || valid_check(number[index - 1], i + '0', a[index - 1])) {
+			check[i] = true;
+			go(index + 1, number + to_string(i));
+			check[i] = false;
+		}
+	}
+}
+
+void solve() {
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	go();
+
+	auto minmax = minmax_element(answer.begin(), answer.end());
+	cout << *minmax.second << '\n';
+	cout << *minmax.first << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
