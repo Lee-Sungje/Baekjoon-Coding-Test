e0959bf8ca17cedc9e554df8aaed820a0e23d184
Lee-Sungje <dtw2813@gmail.com>
2020-10-09 21:45:51 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·13023_ABCDE(Refactoring) - https://www.acmicpc.net/problem/13023
    ·13023_ABCDE - https://www.acmicpc.net/problem/13023

---
+++ 그래프/13023_ABCDE(Refactoring).cpp
@@ -0,0 +1,50 @@
+﻿/* BOJ - 13023 ABCDE(Refactoring) */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> list[2000];
+bool visit[2000];
+bool answer;
+
+void dfs(int index, int count) {
+	if (count == 5) {
+		answer = true;
+		return;
+	}
+
+	visit[index] = true;
+	for (int i = 0; i < list[index].size(); i++) {
+		if (visit[list[index][i]])	continue;
+		dfs(list[index][i], count + 1);
+		if (answer)	return;
+	}
+	visit[index] = false;
+}
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	
+	for (int i = 0; i < m; i++) {
+		int from, to;
+		cin >> from >> to;
+
+		list[from].push_back(to);
+		list[to].push_back(from);
+	}
+
+	for (int i = 0; i < m; i++) {
+		dfs(i, 1);
+		if (answer) break;
+	}
+	cout << answer << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 그래프/13023_ABCDE.cpp
@@ -0,0 +1,59 @@
+﻿/* BOJ - 13023 ABCDE */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool matrix[2000][2000];		// 인접행렬
+vector<int> list[2000];			// 인접리스트
+vector<pair<int, int>> edges;	// 간선리스트
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		int from, to;
+		cin >> from >> to;
+
+		matrix[from][to] = matrix[to][from] = true;
+
+		edges.push_back({ from, to });
+		edges.push_back({ to, from });
+
+		list[from].push_back(to);
+		list[to].push_back(from);
+	}
+
+	m *= 2;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) {
+			// A -> B
+			int A = edges[i].first;
+			int B = edges[i].second;
+			// C -> D
+			int C = edges[j].first;
+			int D = edges[j].second;
+			if (A == B || A == C || A == D || B == C || B == D || C == D)
+				continue;
+			// B -> C
+			if (!matrix[B][C])
+				continue;
+			// D -> E
+			for (int E : list[D]) {
+				if (A == E || B == E || C == E || D == E)
+					continue;
+				cout << 1 << '\n';
+				return;
+			}
+		}
+	}
+	cout << 0 << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
