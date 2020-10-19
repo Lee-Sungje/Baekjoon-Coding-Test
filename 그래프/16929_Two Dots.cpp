1c962aed8a43cc71aff2f8fde77170cba5bd553a
Lee-Sungje <dtw2813@gmail.com>
2020-10-19 21:17:25 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·16929_Two Dots - https://www.acmicpc.net/problem/16929
    ·7562_나이트의 이동 - https://www.acmicpc.net/problem/7562

---
+++ 그래프/16929_Two Dots.cpp
@@ -0,0 +1,60 @@
+﻿/* BOJ - 16929 Two Dots */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+constexpr int BOARD_SIZE = 50;
+constexpr int DIRECTION = 4;
+constexpr int DIRECTION_X[] = { -1, 0, 1, 0 };
+constexpr int DIRECTION_Y[] = { 0, 1, 0, -1 };
+
+int n, m;
+char board[BOARD_SIZE][BOARD_SIZE];
+bool isVisit[BOARD_SIZE][BOARD_SIZE];
+int dist[BOARD_SIZE][BOARD_SIZE];
+
+void input() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		for (int j = 0; j < m; j++)
+			board[i][j] = str[j];
+	}
+}
+
+bool dfs(int x, int y, int count, char color) {
+	if (isVisit[x][y]) return count - dist[x][y] >= 4;
+	isVisit[x][y] = true;
+	dist[x][y] = count;
+
+	for (int d = 0; d < DIRECTION; d++) {
+		int nextX = x + DIRECTION_X[d];
+		int nextY = y + DIRECTION_Y[d];
+		if (0 <= nextX && nextX < n && 0 <= nextY && nextY < m) {
+			if (board[nextX][nextY] == color)
+				if (dfs(nextX, nextY, count + 1, color))
+					return true;
+		}
+	}
+	return false;
+}
+
+bool solve() {
+	input();
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (!isVisit[i][j])
+				if (dfs(i, j, 0, board[i][j]))
+					return true;
+	return false;
+}
+
+int main(void) {
+	if (solve())	cout << "Yes\n";
+	else			cout << "No\n";
+
+	return 0;
+}
---
+++ 그래프/7562_나이트의 이동.cpp
@@ -0,0 +1,74 @@
+﻿/* BOJ - 7562 나이트의 이동 */
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+constexpr int BOARD_SIZE = 300;
+constexpr int DIRECTION = 8;
+constexpr int DIRECTION_X[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
+constexpr int DIRECTION_Y[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+int l;
+bool isVisit[BOARD_SIZE][BOARD_SIZE];
+int answer[BOARD_SIZE][BOARD_SIZE];
+pair<int, int> current;
+pair<int, int> target;
+
+void init() {
+	for (int i = 0; i < l; i++) {
+		for (int j = 0; j < l; j++) {
+			isVisit[i][j] = false;
+			answer[i][j] = 0;
+		}
+	}
+}
+
+void input() {
+	cin >> l;
+	cin >> current.first >> current.second;
+	cin >> target.first >> target.second;
+}
+
+void bfs(int x, int y) {
+	queue<pair<int, int>> q;
+	q.push({ x, y });
+	isVisit[x][y] = true;
+	answer[x][y] = 0;
+
+	while (!q.empty()) {
+		int currentX = q.front().first;
+		int currentY = q.front().second;
+		q.pop();
+		for (int d = 0; d < DIRECTION; d++) {
+			int nextX = currentX + DIRECTION_X[d];
+			int nextY = currentY + DIRECTION_Y[d];
+
+			if (0 <= nextX && nextX < l && 0 <= nextY && nextY < l) {
+				if (!isVisit[nextX][nextY]) {
+					q.push({ nextX, nextY });
+					isVisit[nextX][nextY] = true;
+					answer[nextX][nextY] = answer[currentX][currentY] + 1;
+				}
+			}
+		}
+	}
+}
+
+void solve() {
+	int t;
+	cin >> t;
+	while (t--) {
+		init();
+		input();
+		bfs(current.first, current.second);
+		cout << answer[target.first][target.second] << '\n';
+	}
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
