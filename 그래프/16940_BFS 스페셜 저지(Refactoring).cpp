b845bebb8a6195092119deac567214893b7b227a
Lee-Sungje <dtw2813@gmail.com>
2020-10-21 23:11:32 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·16940_BFS 스페셜 저지(Refactoring) - https://www.acmicpc.net/problem/16940
    ·16940_BFS 스페셜 저지 - https://www.acmicpc.net/problem/16940

---
+++ 그래프/16940_BFS 스페셜 저지(Refactoring).cpp
@@ -0,0 +1,70 @@
+﻿/* BOJ - 16940 BFS 스페셜 저지(Refactoring) */
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+constexpr int VERTEX_SIZE = 100'000;
+
+int n;
+vector<int> adj[VERTEX_SIZE];
+vector<int> sequence;
+vector<int> result;
+bool isVisit[VERTEX_SIZE];
+
+void input() {
+	cin >> n;
+	for (int i = 0; i < n - 1; i++) {
+		int from, to;
+		cin >> from >> to;
+		adj[from - 1].push_back(to - 1);
+		adj[to - 1].push_back(from - 1);
+	}
+	sequence = vector<int>(n);
+	vector<int> order(n);
+	for (int i = 0; i < n; i++) {
+		cin >> sequence[i];
+		sequence[i] -= 1;
+		order[sequence[i]] = i;
+	}
+
+	// sequence순으로 인접 리스트 정렬
+	for (int i = 0; i < n; i++) {
+		sort(adj[i].begin(), adj[i].end(), [&](const int &first, const int &second) {
+			return order[first] < order[second];
+		});
+	}
+}
+
+void bfs(int start = 0) {
+	queue<int> q;
+	q.push(start);
+	isVisit[start] = true;
+	result.push_back(start);
+
+	while (!q.empty()) {
+		int current = q.front(); q.pop();
+		for (int next : adj[current]) {
+			if (!isVisit[next]) {
+				q.push(next);
+				isVisit[next] = true;
+				result.push_back(next);
+			}
+		}
+	}
+}
+
+void solve() {
+	input();
+	bfs();
+	cout << (result == sequence) ? 1 : 0 << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 그래프/16940_BFS 스페셜 저지.cpp
@@ -0,0 +1,73 @@
+﻿/* BOJ - 16940 BFS 스페셜 저지 */
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+constexpr int VERTEX_SIZE = 100'000;
+
+int n;
+vector<int> adj[VERTEX_SIZE];
+vector<int> sequence;
+int parent[VERTEX_SIZE];
+bool isVisit[VERTEX_SIZE];
+
+void input() {
+	cin >> n;
+	for (int i = 0; i < n - 1; i++) {
+		int from, to;
+		cin >> from >> to;
+		adj[from - 1].push_back(to - 1);
+		adj[to - 1].push_back(from - 1);
+	}
+	sequence = vector<int>(n);
+	for (int i = 0; i < n; i++) {
+		cin >> sequence[i];
+		sequence[i] -= 1;
+	}
+}
+
+bool bfs(int start = 0) {
+	queue<int> q;
+	q.push(start);
+	isVisit[start] = true;
+
+	int index = 1;
+	for (int i = 0; i < n; i++) {
+		if (q.empty()) return false;
+
+		int current = q.front(); q.pop();
+		if (current != sequence[i]) return false;
+
+		int count = 0;
+		for (int vertex : adj[current]) {
+			if (!isVisit[vertex]) {
+				parent[vertex] = current;
+				count += 1;
+			}
+		}
+
+		for (int j = 0; j < count; j++) {
+			if (index + j >= n || parent[sequence[index + j]] != current)
+				return false;
+			q.push(sequence[index + j]);
+			isVisit[sequence[index + j]] = true;
+		}
+
+		index += count;
+	}
+	return true;
+}
+
+void solve() {
+	input();
+	cout << bfs() ? 1 : 0 << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
