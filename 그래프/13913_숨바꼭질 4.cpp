2a1d6fbd512acd8d04a14921470c1f9f998f2210
Lee-Sungje <dtw2813@gmail.com>
2020-10-25 21:56:11 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·13913_숨바꼭질 4 - https://www.acmicpc.net/problem/13913
    ·14226_이모티콘 - https://www.acmicpc.net/problem/14226

---
+++ 그래프/13913_숨바꼭질 4.cpp
@@ -0,0 +1,75 @@
+﻿/* BOJ - 13913 숨바꼭질 4 */
+
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+constexpr int MAX_SIZE = 100'001;
+constexpr int MOVE[] = { -1, 1, 2 };
+
+int n, k;
+bool isVisit[MAX_SIZE];
+int dist[MAX_SIZE];
+int from[MAX_SIZE];
+stack<int> route;
+
+void trace() {
+	route.push(k);
+	int previous = from[k];
+	while (previous != -1) {
+		route.push(previous);
+		previous = from[previous];
+	}
+}
+
+void print() {
+	cout << dist[k] << '\n';
+	while (!route.empty()) {
+		cout << route.top() << ' ';
+		route.pop();
+	}
+	cout << '\n';
+}
+
+void bfs() {
+	queue<int> q;
+	q.push(n);
+	isVisit[n] = true;
+	dist[n] = 0;
+	from[n] = -1;
+
+	while (!q.empty()) {
+		int current = q.front();
+		q.pop();
+
+		for (int m = 0; m < 3; m++) {
+			int next;
+			if (m == 2) next = current * MOVE[m];
+			else		next = current + MOVE[m];
+
+			if (0 <= next && next < MAX_SIZE) {
+				if (!isVisit[next]) {
+					q.push(next);
+					isVisit[next] = true;
+					dist[next] = dist[current] + 1;
+					from[next] = current;
+				}
+			}
+		}
+	}
+}
+
+void solve() {
+	cin >> n >> k;
+	bfs();
+	trace();
+	print();
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 그래프/14226_이모티콘.cpp
@@ -0,0 +1,54 @@
+﻿/* BOJ - 14226 이모티콘 */
+
+#include <iostream>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+constexpr int MAX_SIZE = 1001;
+constexpr int OPERATION = 3;
+
+int s;
+int dist[MAX_SIZE][MAX_SIZE];
+
+void bfs() {
+	queue<pair<int, int>> q;
+	q.push({ 1, 0 });
+	dist[1][0] = 0;
+
+	while (!q.empty()) {
+		int emoticon = q.front().first;
+		int clipboard = q.front().second;
+		q.pop();
+
+		if (emoticon == s) {
+			cout << dist[emoticon][clipboard] << '\n';
+			return;
+		}
+
+		int operEmoticon[] = { emoticon, emoticon + clipboard, emoticon - 1 };
+		int operClipboard[] = { emoticon, clipboard, clipboard };
+		for (int op = 0; op < OPERATION; op++) {
+			int e = operEmoticon[op], c = operClipboard[op];
+			if (0 <= e && e < MAX_SIZE) {
+				if (dist[e][c] == -1) {
+					q.push({ e, c });
+					dist[e][c] = dist[emoticon][clipboard] + 1;
+				}
+			}
+		}
+	}
+}
+
+void solve() {
+	cin >> s;
+	memset(dist, -1, sizeof(dist));
+	bfs();
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
