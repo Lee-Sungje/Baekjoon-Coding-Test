bb8b6a08e4652f88e5d9499c75cba6f90a0cad4a
Lee-Sungje <dtw2813@gmail.com>
2020-10-28 23:17:39 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·11725_트리의 부모 찾기 - https://www.acmicpc.net/problem/11725

---
+++ 트리/11725_트리의 부모 찾기.cpp
@@ -0,0 +1,61 @@
+﻿/* BOJ - 11725 트리의 부모 찾기 */
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+constexpr int MAX_SIZE = 100'001;
+constexpr int ROOT = 1;
+
+int n;
+vector<int> adj[MAX_SIZE];
+int parent[MAX_SIZE];
+
+void input() {
+	cin >> n;
+
+	for (int i = 0; i < n - 1; i++) {
+		int from, to;
+		cin >> from >> to;
+		adj[from].push_back(to);
+		adj[to].push_back(from);
+	}
+}
+
+void output() {
+	for (int i = 2; i <= n; i++)
+		cout << parent[i] << '\n';
+}
+
+void bfs(int root) {
+	queue<int> q;
+	q.push(root);
+	parent[root] = -1;
+
+	while (!q.empty()) {
+		int node = q.front(); q.pop();
+
+		for (int i = 0; i < adj[node].size(); i++) {
+			int nextNode = adj[node][i];
+
+			if (parent[nextNode] == 0) {
+				q.push(nextNode);
+				parent[nextNode] = node;
+			}
+		}
+	}
+}
+
+void solve() {
+	input();
+	bfs(ROOT);
+	output();
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
