19f399071a22dc8a5461ddfd5fc7fc48d0a39fce
Lee-Sungje <dtw2813@gmail.com>
2020-10-30 21:50:19 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1012_유기농 배추 - https://www.acmicpc.net/problem/1012
    ·2606_바이러스 - https://www.acmicpc.net/problem/2606

---
+++ 그래프/1012_유기농 배추.cpp
@@ -0,0 +1,79 @@
+﻿/* BOJ - 1012 유기농 배추 */
+
+#include <cstring>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+constexpr int MAP_SIZE = 50;
+constexpr int DIRECTION = 4;
+constexpr int DIRECTION_X[] = { 0, 1, 0, -1 };
+constexpr int DIRECTION_Y[] = { -1, 0, 1, 0 };
+
+int m, n, k;
+bool map[MAP_SIZE][MAP_SIZE];
+int scope[MAP_SIZE][MAP_SIZE];
+
+void init() {
+	memset(map, false, sizeof(map));
+	memset(scope, 0, sizeof(scope));
+}
+
+void input() {
+	cin >> m >> n >> k;
+
+	for (int count = 0; count < k; count++) {
+		int x, y;
+		cin >> x >> y;
+		map[y][x] = true;
+	}
+}
+
+void bfs(int y, int x, int worm) {
+	queue<pair<int, int>> q;
+	q.push({ y, x });
+	scope[y][x] = worm;
+
+	while (!q.empty()) {
+		int currentY = q.front().first;
+		int currentX = q.front().second;
+		q.pop();
+
+		for (int d = 0; d < DIRECTION; d++) {
+			int nextY = currentY + DIRECTION_Y[d];
+			int nextX = currentX + DIRECTION_X[d];
+
+			if (0 <= nextY && nextY < n && 0 <= nextX && nextX < m) {
+				if (map[nextY][nextX] && scope[nextY][nextX] != worm) {
+					q.push({ nextY, nextX });
+					scope[nextY][nextX] = worm;
+				}
+			}
+		}
+	}
+}
+
+void solve() {
+	int testcase;
+	cin >> testcase;
+
+	while (testcase--) {
+		init();
+		input();
+		int worm = 0;
+
+		for (int y = 0; y < n; y++)
+			for (int x = 0; x < m; x++)
+				if (map[y][x] && scope[y][x] == 0)
+					bfs(y, x, ++worm);
+
+		cout << worm << '\n';
+	}
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 그래프/2606_바이러스.cpp
@@ -0,0 +1,57 @@
+﻿/* BOJ - 2606 바이러스 */
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+constexpr int COMPUTER_SIZE = 100;
+
+int node, edge;
+vector<int> computer[COMPUTER_SIZE];
+bool isVisit[COMPUTER_SIZE];
+
+void input() {
+	cin >> node;
+	cin >> edge;
+	for (int n = 0; n < edge; n++) {
+		int from, to;
+		cin >> from >> to;
+		computer[from - 1].push_back(to - 1);
+		computer[to - 1].push_back(from - 1);
+	}
+}
+
+int bfs(int start) {
+	queue<int> q;
+	q.push(start);
+	isVisit[start] = true;
+
+	int answer = 0;
+	while (!q.empty()) {
+		int current = q.front();
+		q.pop();
+
+		for (int n : computer[current]) {
+			if (!isVisit[n]) {
+				q.push(n);
+				isVisit[n] = true;
+				answer += 1;
+			}
+		}
+	}
+
+	return answer;
+}
+
+void solve() {
+	input();
+	cout << bfs(0) << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
