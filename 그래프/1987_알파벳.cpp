a2b0bc9f43282286b7e0bd1e976a469a241f5cd0
Lee-Sungje <dtw2813@gmail.com>
2020-11-02 23:33:50 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1987_알파벳 - https://www.acmicpc.net/problem/1987
    ·2583_영역 구하기 - https://www.acmicpc.net/problem/2583

---
+++ 그래프/1987_알파벳.cpp
@@ -0,0 +1,57 @@
+﻿/* BOJ - 1987 알파벳 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+constexpr int BOARD_SIZE = 20;
+constexpr int ALPHABET_LENGTH = 26;
+constexpr int DIRECTION = 4;
+constexpr int DIRECTION_X[] = { -1, 0, 1, 0 };
+constexpr int DIRECTION_Y[] = { 0, 1, 0, -1 };
+
+int r, c, answer = 1;
+char board[BOARD_SIZE][BOARD_SIZE];
+bool isVisit[ALPHABET_LENGTH];
+
+void input() {
+	cin >> r >> c;
+	
+	for (int x = 0; x < r; x++) {
+		string str;
+		cin >> str;
+		for (int y = 0; y < c; y++)
+			board[x][y] = str[y];
+	}
+}
+
+void dfs(int x, int y, int count) {
+	answer = answer < count ? count : answer;
+
+	for (int d = 0; d < DIRECTION; d++) {
+		int nextX = x + DIRECTION_X[d];
+		int nextY = y + DIRECTION_Y[d];
+
+		if (0 <= nextX && nextX < r && 0 <= nextY && nextY < c) {
+			if (!isVisit[board[nextX][nextY] - 'A']) {
+				isVisit[board[nextX][nextY] - 'A'] = true;
+				dfs(nextX, nextY, count + 1);
+				isVisit[board[nextX][nextY] - 'A'] = false;
+			}
+		}
+	}
+}
+
+void solve() {
+	input();
+	isVisit[board[0][0] - 'A'] = true;
+	dfs(0, 0, 1);
+	cout << answer << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 그래프/2583_영역 구하기.cpp
@@ -0,0 +1,84 @@
+﻿/* BOJ - 2583 영역 구하기 */
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+constexpr int MAX_SIZE = 100;
+constexpr int DIRECTION = 4;
+constexpr int DIRECTION_X[] = { -1, 0, 1, 0 };
+constexpr int DIRECTION_Y[] = { 0, 1, 0, -1 };
+
+int m, n, k;
+bool graph[MAX_SIZE][MAX_SIZE];
+int area[MAX_SIZE][MAX_SIZE];
+vector<int> answers;
+
+void input() {
+	cin >> m >> n >> k;
+	for (int i = 0; i < k; i++) {
+		int y1, x1, y2, x2;
+		cin >> y1 >> x1 >> y2 >> x2;
+
+		for (int x = x1; x < x2; x++)
+			for (int y = y1; y < y2; y++)
+				graph[x][y] = true;
+	}
+}
+
+void output() {
+	cout << answers.size() << '\n';
+	for (int answer : answers)
+		cout << answer << ' ';
+	cout << '\n';
+}
+
+int bfs(int x, int y, int color) {
+	queue<pair<int, int>> q;
+	q.push({ x, y });
+	area[x][y] = color;
+	int count = 1;
+
+	while (!q.empty()) {
+		int currentX = q.front().first;
+		int currentY = q.front().second;
+		q.pop();
+
+		for (int d = 0; d < DIRECTION; d++) {
+			int nextX = currentX + DIRECTION_X[d];
+			int nextY = currentY + DIRECTION_Y[d];
+
+			if (0 <= nextX && nextX < m && 0 <= nextY && nextY < n) {
+				if (!graph[nextX][nextY] && area[nextX][nextY] != color) {
+					q.push({ nextX, nextY });
+					area[nextX][nextY] = color;
+					count += 1;
+				}
+			}
+		}
+	}
+
+	return count;
+}
+
+void solve() {
+	input();
+
+	int color = 0;
+	for (int x = 0; x < m; x++)
+		for (int y = 0; y < n; y++)
+			if (!graph[x][y] && area[x][y] == 0)
+				answers.push_back(bfs(x, y, ++color));
+
+	sort(answers.begin(), answers.end());
+	output();
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
