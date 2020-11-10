19c361df1ef9edafdc4a69a998feb1a30eebb196
Lee-Sungje <dtw2813@gmail.com>
2020-11-10 23:11:01 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·12865_평범한 배낭 - https://www.acmicpc.net/problem/12865

---
+++ 다이나믹 프로그래밍/배낭 문제/12865_평범한 배낭.cpp
@@ -0,0 +1,53 @@
+﻿/* BOJ - 12865 평범한 배낭 */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Item {
+	int value;
+	int weight;
+
+	Item(int _value, int _weight) : value(_value), weight(_weight) {}
+};
+
+constexpr int MAX_WEIGHT = 100'001;
+
+int n, k;
+int knapsack[MAX_WEIGHT];
+vector<Item> items;
+
+void input() {
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		int w, v;
+		cin >> w >> v;
+		items.push_back(Item(v, w));
+	}
+}
+
+void pack() {
+	for (int index = 0; index < n; index++) {
+		for (int weight = k; weight > 0; weight--) {
+			if (items[index].weight <= weight) {
+				knapsack[weight] = max(knapsack[weight],
+					knapsack[weight - items[index].weight] + items[index].value);
+			}
+		}
+	}
+}
+
+void solve() {
+	input();
+	pack();
+
+	cout << knapsack[k] << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
