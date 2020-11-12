edfeda4d907381aa0bada07c75194d1b6e018f60
Lee-Sungje <dtw2813@gmail.com>
2020-11-12 22:59:18 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1010_다리 놓기 - https://www.acmicpc.net/problem/1010
    ·2775_부녀회장이 될테야 - https://www.acmicpc.net/problem/2775

---
+++ 조합론/1010_다리 놓기.cpp
@@ -0,0 +1,36 @@
+﻿/* BOJ - 1010 다리 놓기 */
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long long ULL;
+
+ULL answers[31][31];
+
+ULL dp(int n, int r) {
+	if (r == 0 || r == n) return answers[r][n] = 1;
+
+	if (answers[r][n] == 0)
+		answers[r][n] = dp(n - 1, r - 1) + dp(n - 1, r);
+
+	return answers[r][n];
+}
+
+void solve() {
+	int testcase;
+	cin >> testcase;
+
+	while (testcase--) {
+		int n, m;
+		cin >> n >> m;
+		cout << dp(m, n) << '\n';
+	}
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 조합론/2775_부녀회장이 될테야.cpp
@@ -0,0 +1,41 @@
+﻿/* BOJ - 2775 부녀회장이 될테야 */
+
+#include <iostream>
+
+using namespace std;
+
+constexpr int MAX_SIZE = 15;
+
+int apartment[MAX_SIZE][MAX_SIZE];
+
+void census() {
+	for (int i = 0; i < MAX_SIZE; i++) {
+		for (int j = 0; j < MAX_SIZE; j++) {
+			if (i == 0)
+				apartment[i][j] = j;
+			else if (j < 2)
+				apartment[i][j] = j;
+			else
+				apartment[i][j] = apartment[i - 1][j] + apartment[i][j - 1];
+		}
+	}
+}
+
+void solve() {
+	census();
+
+	int testcase;
+	cin >> testcase;
+	while (testcase--) {
+		int n, k;
+		cin >> n;
+		cin >> k;
+		cout << apartment[n][k] << '\n';
+	}
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
