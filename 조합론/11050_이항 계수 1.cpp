746168ebe9e269b36600dfbc9ce9327a76e60f5b
Lee-Sungje <dtw2813@gmail.com>
2020-11-13 23:40:06 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·11050_이항 계수 1 - https://www.acmicpc.net/problem/11050
    ·1256_사전 - https://www.acmicpc.net/problem/1256

---
+++ 조합론/11050_이항 계수 1.cpp
@@ -0,0 +1,28 @@
+﻿/* BOJ - 11050 이항 계수 1 */
+
+#include <iostream>
+
+using namespace std;
+
+int dp[11][11];
+
+int combination(int n, int k) {
+	if (k == 0 || k == n) return dp[k][n] = 1;
+
+	if (dp[k][n] == 0)
+		dp[k][n] = combination(n - 1, k - 1) + combination(n - 1, k);
+
+	return dp[k][n];
+}
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+	cout << combination(n, k) << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 조합론/1256_사전.cpp
@@ -0,0 +1,62 @@
+﻿/* BOJ - 1256 사전 */
+
+#include <iostream>
+
+using namespace std;
+
+constexpr int MAX = 1'000'000'000;
+int c[101][201];
+
+void combination(int n, int m) {
+	for (int i = 0; i <= n; i++) {
+		for (int j = i; j <= n + m; j++) {
+			if (i == 0 || i == j)
+				c[i][j] = 1;
+			else
+				c[i][j] = c[i - 1][j - 1] + c[i][j - 1];
+
+			if (c[i][j] >= MAX) c[i][j] = MAX;
+		}
+	}
+}
+
+void output(int n, int m, int k) {
+	if (n == 0) {
+		for (int i = m; i > 0; i--)
+			cout << 'z';
+		return;
+	}
+
+	if (m == 0) {
+		for (int i = n; i > 0; i--)
+			cout << 'a';
+		return;
+	}
+
+	if (k <= c[n - 1][n + m - 1]) {
+		cout << 'a';
+		output(n - 1, m, k);
+	} else {
+		cout << 'z';
+		output(n, m - 1, k - c[n - 1][n + m - 1]);
+	}
+}
+
+void solve() {
+	int n, m, k;
+	cin >> n >> m >> k;
+	combination(n, m);
+
+	if (c[n][n + m] < k) {
+		cout << -1 << '\n';
+		return;
+	} else {
+		output(n, m, k);
+	}
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
