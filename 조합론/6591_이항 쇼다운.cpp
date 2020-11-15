2e6c62d323e17801562ae9006b84869f991a4a5e
Lee-Sungje <dtw2813@gmail.com>
2020-11-15 23:44:29 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·6591_이항 쇼다운 - https://www.acmicpc.net/problem/6591
    ·8320_직사각형을 만드는 방법 - https://www.acmicpc.net/problem/8320

---
+++ 조합론/6591_이항 쇼다운.cpp
@@ -0,0 +1,31 @@
+﻿/* BOJ 6591 - 이항 쇼다운 */
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+	while (true) {
+		long long answer = 1;
+		int n, r;
+		cin >> n >> r;
+
+		if (n == 0 && r == 0) break;
+
+		r = min(r, n - r);
+
+		for (int i = 1; i <= r; i++) {
+			answer *= n;
+			answer /= i;
+			n -= 1;
+		}
+		cout << answer << '\n';
+	}
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 조합론/8320_직사각형을 만드는 방법.cpp
@@ -0,0 +1,24 @@
+﻿/* BOJ - 8320 직사각형을 만드는 방법 */
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+	int n, range;
+	cin >> n;
+
+	range = (int)sqrt(n);
+	int answer = 0;
+	for (int i = 1; i <= range; i++)
+		answer += (n - (int)pow(i, 2)) / i + 1;
+
+	cout << answer << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
