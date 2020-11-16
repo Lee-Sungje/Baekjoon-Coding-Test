2656229c9f7b700a7bc09c4daee58e122a221e82
Lee-Sungje <dtw2813@gmail.com>
2020-11-16 22:42:15 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·16395_파스칼의 삼각형 - https://www.acmicpc.net/problem/16395
    ·16561_3의 배수 - https://www.acmicpc.net/problem/16561

---
+++ 조합론/16395_파스칼의 삼각형.cpp
@@ -0,0 +1,32 @@
+﻿/* BOJ - 16395 파스칼의 삼각형 */
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long long ULL;
+constexpr int MAX_SIZE = 31;
+
+ULL pascal[MAX_SIZE][MAX_SIZE];
+
+ULL combination(int n, int r) {
+	if (r == 0 || n == r) return pascal[r][n] = 1;
+
+	if (pascal[r][n] == 0)
+		pascal[r][n] = combination(n - 1, r) + combination(n - 1, r - 1);
+
+	return pascal[r][n];
+}
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+	cout << combination(n - 1, k - 1) << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 조합론/16561_3의 배수.cpp
@@ -0,0 +1,20 @@
+﻿/* BOJ - 16561 3의 배수 */
+
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+	int n, answer;
+	cin >> n;
+
+	n /= 3;
+	answer = (n - 1) * (n - 2) / 2;
+	cout << answer << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
