a4745cc671d665597b35d9a1d1c5520f98a8cf19
Lee-Sungje <dtw2813@gmail.com>
2020-11-27 13:40:44 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·10773_제로 - https://www.acmicpc.net/problem/10773

---
+++ 문자열/10773_제로.cpp
@@ -0,0 +1,38 @@
+﻿/* BOJ - 10773 제로 */
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<int> s;
+
+void input() {
+	int k, num;
+
+	cin >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> num;
+		if (!s.empty() && num == 0)
+			s.pop();
+		else
+			s.push(num);
+	}
+}
+
+void solve() {
+	int answer = 0;
+
+	input();
+	while (!s.empty()) {
+		answer += s.top();
+		s.pop();
+	}
+	cout << answer << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
