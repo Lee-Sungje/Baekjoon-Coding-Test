2d3409c7c24b2f54e68fea6161009f8d893cc17a
Lee-Sungje <dtw2813@gmail.com>
2020-11-28 22:42:16 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·2941_크로아티아 알파벳 - https://www.acmicpc.net/problem/2941

---
+++ 문자열/2941_크로아티아 알파벳.cpp
@@ -0,0 +1,30 @@
+﻿/* BOJ - 2941 크로아티아 알파벳 */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> croatiaAlphabet = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+string replaceString = "*";
+
+void solve() {
+	string str;
+	cin >> str;
+
+	for (string alphabet : croatiaAlphabet) {
+		int pos = str.find(alphabet);
+		while (pos != string::npos) {
+			str.replace(pos, alphabet.size(), replaceString);
+			pos = str.find(alphabet);
+		}
+	}
+	cout << str.size() << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
