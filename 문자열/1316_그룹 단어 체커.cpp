cf36bd1bc43dbc522b18bcc49abb852e2e43dc1f
Lee-Sungje <dtw2813@gmail.com>
2020-12-01 16:30:45 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1316_그룹 단어 체커 - https://www.acmicpc.net/problem/1316

---
+++ 문자열/1316_그룹 단어 체커.cpp
@@ -0,0 +1,46 @@
+﻿/* BOJ - 1316 그룹 단어 체커 */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+constexpr int ALPHABET = 26;
+
+bool checker(string &str) {
+	vector<bool> check(ALPHABET, false);
+	char previous;
+
+	for (char c : str) {
+		if (!check[c - 'a'] || c == previous) {
+			check[c - 'a'] = true;
+			previous = c;
+		} else {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void solve() {
+	int testcase;
+	cin >> testcase;
+
+	int answer = 0;
+	while (testcase--) {
+		string str;
+		cin >> str;
+
+		if (checker(str))
+			answer += 1;
+	}
+	cout << answer << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
