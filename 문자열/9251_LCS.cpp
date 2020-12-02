c2addbece79434a8b84fbe2a34cd3684e700060a
Lee-Sungje <dtw2813@gmail.com>
2020-12-02 20:47:35 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·9251_LCS - https://www.acmicpc.net/problem/9251

---
+++ 문자열/9251_LCS.cpp
@@ -0,0 +1,39 @@
+﻿/* BOJ - 9251 LCS */
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+constexpr int MAX_LENGTH = 1'001;
+
+int dp[MAX_LENGTH][MAX_LENGTH];
+
+int LCS(string &str1, string &str2) {	
+	int r, c;
+	for (r = 1; r <= str1.size(); r++) {
+		for (c = 1; c <= str2.size(); c++) {
+			if (str1[r - 1] == str2[c - 1])
+				dp[r][c] = dp[r - 1][c - 1] + 1;
+			else
+				dp[r][c] = max(dp[r][c - 1], dp[r - 1][c]);
+		}
+	}
+
+	return dp[r - 1][c - 1];
+}
+
+void solve() {
+	string str1, str2;
+	cin >> str1;
+	cin >> str2;
+	
+	cout << LCS(str1, str2) << '\n';
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
