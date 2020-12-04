d7ee6852149db1123111bf9a7abdd4fe559fde6d
Lee-Sungje <dtw2813@gmail.com>
2020-12-04 23:29:14 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1764_듣보잡 - https://www.acmicpc.net/problem/1764
    ·2577_숫자의 개수 - https://www.acmicpc.net/problem/2577

---
+++ 문자열/1764_듣보잡.cpp
@@ -0,0 +1,54 @@
+﻿/* BOJ - 1764 듣보잡 */
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> noHear, noSee;
+
+void input() {
+	int n, m;
+	cin >> n >> m;
+
+	string name;
+	for (int i = 0; i < m; i++) {
+		cin >> name;
+		noHear.push_back(name);
+	}
+
+	for (int i = 0; i < n; i++) {
+		cin >> name;
+		noSee.push_back(name);
+	}
+}
+
+void output(vector<string> &answer) {
+	cout << answer.size() << '\n';
+
+	for (string name : answer)
+		cout << name << '\n';
+}
+
+void solve() {
+	input();
+
+	sort(noHear.begin(), noHear.end());
+	sort(noSee.begin(), noSee.end());
+
+	vector<string> answer;
+	for (string name : noSee) {
+		if (binary_search(noHear.begin(), noHear.end(), name))
+			answer.push_back(name);
+	}
+	
+	output(answer);
+}
+
+int main(void) {
+	solve();
+
+	return 0;
+}
---
+++ 문자열/2577_숫자의 개수.cpp
@@ -0,0 +1,27 @@
+﻿/* BOJ - 2577 숫자의 개수 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+constexpr int DECIMAL = 10;
+
+void solve() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	
+	int cnt[DECIMAL] = { 0 };
+	string result = to_string(a * b * c);
+	
+	for (char number : result)
+		cnt[number - '0'] += 1;
+
+	for (int i = 0; i < DECIMAL; i++)
+		cout << cnt[i] << '\n';
+}
+
+int main() {
+	solve();
+
+	return 0;
+}
