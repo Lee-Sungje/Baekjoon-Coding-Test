f15a9bbc71bab2f856096245b08b38e6a5e08fc7
Lee-Sungje <dtw2813@gmail.com>
2020-12-06 16:20:46 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·10809_알파벳 찾기 - https://www.acmicpc.net/problem/10809

---
+++ 문자열/10809_알파벳 찾기.cpp
@@ -0,0 +1,31 @@
+﻿/* BOJ - 알파벳 찾기 */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+constexpr int ALPHABET = 26;
+
+void solve() {
+	string word;
+	cin >> word;
+
+	vector<int> answer(ALPHABET, -1);
+	for (int index = 0; index < word.length(); index++) {
+		char ch = word[index];
+
+		if (answer[ch - 'a'] == -1)
+			answer[ch - 'a'] = index;
+	}
+
+	for (int n : answer)
+		cout << n << ' ';
+}
+
+int main() {
+	solve();
+
+	return 0;
+}
