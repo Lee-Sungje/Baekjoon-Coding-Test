a05825c2207f4a9a7773a18cb70e29432216ecba
Lee-Sungje <dtw2813@gmail.com>
2020-12-08 00:39:37 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·11720_숫자의 합 - https://www.acmicpc.net/problem/11720

---
+++ 문자열/11720_숫자의 합.cpp
@@ -0,0 +1,26 @@
+﻿/* BOJ - 11720 숫자의 합 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+	int n;
+	string str;
+
+	cin >> n;
+	cin >> str;
+
+	int sum = 0;
+	for (char number : str)
+		sum += number - '0';
+
+	cout << sum << '\n';
+}
+
+int main() {
+	solve();
+
+	return 0;
+}
