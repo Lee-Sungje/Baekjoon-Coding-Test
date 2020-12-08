94867a638385a07a5d52f56310918519033c5063
Lee-Sungje <dtw2813@gmail.com>
2020-12-09 02:04:03 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·1152_단어의 개수 - https://www.acmicpc.net/problem/1152

---
+++ 문자열/1152_단어의 개수.cpp
@@ -0,0 +1,34 @@
+﻿/* BOJ - 1152 단어의 개수 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+constexpr int SPACE = 0x20;
+
+int wordCount(string& str) {
+	int cnt = 1;
+
+	for (char ch : str) {
+		if (ch == SPACE)
+			cnt += 1;
+	}
+	
+	if (str.front() == SPACE) cnt -= 1;
+	if (str.back() == SPACE) cnt -= 1;
+
+	return cnt;
+}
+
+void solve() {
+	string str;
+	getline(cin, str);
+	cout << wordCount(str) << '\n';
+}
+
+int main() {
+	solve();
+
+	return 0;
+}
