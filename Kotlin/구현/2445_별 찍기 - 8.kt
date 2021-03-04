d2d0d9fb129c782914222825236a039f14cb48ea
Lee-Sungje <dtw2813@gmail.com>
2021-03-04 21:18:55 +0900

    백준알고리즘 코딩테스트 연습 문제
    ·2444_별 찍기 - 7 - https://www.acmicpc.net/problem/2444
    ·2445_별 찍기 - 8 - https://www.acmicpc.net/problem/2445

---
+++ Kotlin/구현/2445_별 찍기 - 8.kt
@@ -0,0 +1,27 @@
+/* BOJ - 2445 별 찍기 - 8 */
+
+package boj
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun makeStar(size: Int) = mutableListOf<String>().apply {
+    for (count in 1..size) {
+        this.add("*".repeat(count) + " ".repeat((size - count) * 2) + "*".repeat(count))
+    }
+    for (index in this.lastIndex - 1 downTo 0) {
+        this.add(this[index])
+    }
+}
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = reader.readLine().toInt()
+
+    makeStar(n).forEach { writer.write("$it\n") }
+    writer.close()
+}
